ffc4cdc2a79a16ea586ab368ae6e9bb86016079b
Sathyanarayanan Govindarajan <gsathya28@gmail.com>
2016-12-05 11:04:52 -0600

    Object Creation Commit

---
+++ DebateTimerApp/debate.swift
@@ -0,0 +1,13 @@
+//
+//  debate.swift
+//  DebateTimerApp
+//
+//  Created by GOVINDARAJAN, SATHYANARAYANAN on 12/5/16.
+//  Copyright © 2016 GOVINDARAJAN, SATHYANARAYANAN. All rights reserved.
+//
+
+import UIKit
+
+class debate: NSObject {
+
+}
---
+++ DebateTimerApp/roundTime.swift
@@ -0,0 +1,13 @@
+//
+//  roundTime.swift
+//  DebateTimerApp
+//
+//  Created by GOVINDARAJAN, SATHYANARAYANAN on 12/5/16.
+//  Copyright © 2016 GOVINDARAJAN, SATHYANARAYANAN. All rights reserved.
+//
+
+import UIKit
+
+class roundTime: NSObject {
+
+}
