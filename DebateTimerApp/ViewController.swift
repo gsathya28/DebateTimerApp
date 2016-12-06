442a42047252888062803b402eb6c939bf025512
BenGoffman <ben@goffman.com>
2016-12-06 10:47:58 -0600

    Connect GUI
    Connected the buttons and labels to the coding part

--- DebateTimerApp/ViewController.swift
+++ DebateTimerApp/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var AppTitle: UILabel!
+    
+    //Mark properties
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func NewDebate(_ sender: AnyObject) {
+    }
+    
+    @IBAction func ContinueDebate(_ sender: AnyObject) {
+    }
+    
+    @IBAction func PreviousDebates(_ sender: AnyObject) {
+    }
+    
 
 }
 
