f5f2704bbdb5166cd74382b7ef8a60158f08d1ac
BenGoffman <ben@goffman.com>
2016-12-08 10:54:12 -0600

    Connect New Debate Button
    Connects the new debate button to a blank screen

--- DebateTimerApp/NewDebateInputPage.swift
+++ DebateTimerApp/NewDebateInputPage.swift
@@ -1,36 +1,25 @@
 //
-//  ViewController.swift
+//  NewDebateInputPage.swift
 //  DebateTimerApp
 //
-//  Created by GOVINDARAJAN, SATHYANARAYANAN on 12/5/16.
+//  Created by GOFFMAN, BENJAMIN on 12/8/16.
 //  Copyright © 2016 GOVINDARAJAN, SATHYANARAYANAN. All rights reserved.
 //
 
+import Foundation
+
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet var AppTitle: UILabel!
+class NewDebateInputPage: UIViewController {
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func NewDebate(_ sender: AnyObject) {
-    }
-    
-    @IBAction func ContinueDebate(_ sender: AnyObject) {
-    }
-    
-    @IBAction func PreviousDebates(_ sender: AnyObject) {
-    }
-    
-
 }
-
