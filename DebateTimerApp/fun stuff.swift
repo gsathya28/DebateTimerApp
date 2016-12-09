8e356f03049bc0c04606aa887842c9f9aa401baa
JacobFl <jacob.flaum@gmail.com>
2016-12-09 10:34:15 -0600

    Dropdown picker thing
    trying to have a drop down and need to figure out how to populate it

---
+++ DebateTimerApp/fun stuff.swift
@@ -0,0 +1,25 @@
+//
+//  fun stuff.swift
+//  DebateTimerApp
+//
+//  Created by FLAUM, JACOB on 12/7/16.
+//  Copyright © 2016 GOVINDARAJAN, SATHYANARAYANAN. All rights reserved.
+//
+
+import UIKit
+
+class FunStuff: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
