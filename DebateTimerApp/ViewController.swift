c32708e4ab5dea0c1abe7c762fcb005f55d62356
BenGoffman <ben@goffman.com>
2016-12-12 11:03:11 -0600

    UIPickerView
    Tried to fix the UIPickerView but it doesn’t work yet. The new debate
    button stopped working now

--- DebateTimerApp/ViewController.swift
+++ DebateTimerApp/ViewController.swift
@@ -8,14 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+
+class ViewController: UIViewController, UIPickerViewDelegate {
+    
     @IBOutlet var AppTitle: UILabel!
     
     @IBOutlet weak var PickerView: UIPickerView!
 
+    var pickerDataSource = ["White", "Red", "Green", "Blue"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerDataSource.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerDataSource[row]
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,6 +49,25 @@ class ViewController: UIViewController {
     @IBAction func PreviousDebates(_ sender: AnyObject) {
     }
     
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        if(row == 0)
+        {
+            self.view.backgroundColor = UIColor.white
+        }
+        else if(row == 1)
+        {
+            self.view.backgroundColor = UIColor.red
+        }
+        else if(row == 2)
+        {
+            self.view.backgroundColor =  UIColor.green
+        }
+        else
+        {
+            self.view.backgroundColor = UIColor.blue
+        }
+    }
 
 }
 
