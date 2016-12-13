29fe2ee7ee982b0210c1394a829c6fee2fecb863
BenGoffman <ben@goffman.com>
2016-12-13 10:33:24 -0600

    Move to input page
    Moved everything that connects to the picker view from the main page to
    the New Debate Input page

--- DebateTimerApp/NewDebateInputPage.swift
+++ DebateTimerApp/NewDebateInputPage.swift
@@ -10,8 +10,10 @@ import Foundation
 
 import UIKit
 
-class NewDebateInputPage: UIViewController {
+
+class NewDebateInputPage: UIViewController, UIPickerViewDelegate {
     
+    var pickerDataSource = ["White", "Red", "Green", "Blue"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,4 +24,37 @@ class NewDebateInputPage: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerDataSource.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerDataSource[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        if(row == 0)
+        {
+            self.view.backgroundColor = UIColor.white
+        }
+        else if(row == 1)
+        {
+            self.view.backgroundColor = UIColor.red
+        }
+        else if(row == 2)
+        {
+            self.view.backgroundColor =  UIColor.green
+        }
+        else
+        {
+            self.view.backgroundColor = UIColor.blue
+        }
+    }
+
 }
--- DebateTimerApp/ViewController.swift
+++ DebateTimerApp/ViewController.swift
@@ -10,31 +10,15 @@ import UIKit
 
 
 
-class ViewController: UIViewController, UIPickerViewDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet var AppTitle: UILabel!
-    
-    @IBOutlet weak var PickerView: UIPickerView!
-
-    var pickerDataSource = ["White", "Red", "Green", "Blue"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerDataSource.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return pickerDataSource[row]
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -49,25 +33,5 @@ class ViewController: UIViewController, UIPickerViewDelegate {
     @IBAction func PreviousDebates(_ sender: AnyObject) {
     }
     
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
-    {
-        if(row == 0)
-        {
-            self.view.backgroundColor = UIColor.white
-        }
-        else if(row == 1)
-        {
-            self.view.backgroundColor = UIColor.red
-        }
-        else if(row == 2)
-        {
-            self.view.backgroundColor =  UIColor.green
-        }
-        else
-        {
-            self.view.backgroundColor = UIColor.blue
-        }
-    }
-
 }
 
