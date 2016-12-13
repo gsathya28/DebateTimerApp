24313088d9c728103791eab17134bdcf9f9edbd8
BenGoffman <ben@goffman.com>
2016-12-13 11:07:28 -0600

    uipicker view problem
    No compile errors, when you look at the screen it only shows the first
    one in the array many times and in one row.

--- DebateTimerApp/NewDebateInputPage.swift
+++ DebateTimerApp/NewDebateInputPage.swift
@@ -6,17 +6,18 @@
 //  Copyright © 2016 GOVINDARAJAN, SATHYANARAYANAN. All rights reserved.
 //
 
-import Foundation
-
 import UIKit
 
 
-class NewDebateInputPage: UIViewController, UIPickerViewDelegate {
+class NewDebateInputPage: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
-    var pickerDataSource = ["White", "Red", "Green", "Blue"]
+    @IBOutlet weak var pickerView: UIPickerView!
+    var pickerDataSource = ["Ben","Sux", "Dick"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        pickerView.dataSource = self
+        pickerView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -25,36 +26,16 @@ class NewDebateInputPage: UIViewController, UIPickerViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return pickerDataSource.count
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerDataSource.count
+        return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return pickerDataSource[row]
     }
     
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
-    {
-        if(row == 0)
-        {
-            self.view.backgroundColor = UIColor.white
-        }
-        else if(row == 1)
-        {
-            self.view.backgroundColor = UIColor.red
-        }
-        else if(row == 2)
-        {
-            self.view.backgroundColor =  UIColor.green
-        }
-        else
-        {
-            self.view.backgroundColor = UIColor.blue
-        }
-    }
-
 }
