f5db32d0bb89b55515dd9440e276ee0c5461807a
BenGoffman <ben@goffman.com>
2016-12-14 10:46:27 -0600

    Picker View complete
    Finished making the picker view for the user to pick which hour to use

--- DebateTimerApp/NewDebateInputPage.swift
+++ DebateTimerApp/NewDebateInputPage.swift
@@ -12,12 +12,12 @@ import UIKit
 class NewDebateInputPage: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     @IBOutlet weak var pickerView: UIPickerView!
-    var pickerDataSource = ["Ben","Sux", "Dick"]
     
+    var pickerData = ["Hour 1", "Hour 2", "Hour 3", "Hour 4", "Hour 5", "Hour 6", "Hour 7"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        pickerView.dataSource = self
         pickerView.delegate = self
+        pickerView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -27,15 +27,15 @@ class NewDebateInputPage: UIViewController, UIPickerViewDataSource, UIPickerView
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return pickerDataSource.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return 1
     }
     
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return pickerDataSource[row]
+        return pickerData[row]
     }
     
 }
