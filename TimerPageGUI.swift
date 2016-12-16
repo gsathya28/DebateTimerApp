c8bcb29c2ca3ecd4cbb2565fbcc60dda1da09e09
JacobFl <jacob.flaum@gmail.com>
2016-12-16 11:08:08 -0600

    Timer Page GUI
    Worked on the first page of Timer Page GUI. I have opening statement
    and the picker and buttons down. NOT FINISHED YET

---
+++ TimerPageGUI.swift
@@ -0,0 +1,28 @@
+//
+//  TimerPageGUI.swift
+//  DebateTimerApp
+//
+//  Created by FLAUM, JACOB on 12/16/16.
+//  Copyright © 2016 GOVINDARAJAN, SATHYANARAYANAN. All rights reserved.
+//
+
+import UIKit
+
+
+
+class TimePageGUI: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
