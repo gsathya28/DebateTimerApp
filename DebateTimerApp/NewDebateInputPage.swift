2c7833bcb285297cd321a0390a7f7975bb52bb4f
BenGoffman <ben@goffman.com>
2016-12-16 10:35:00 -0600

    back button
    Connected the back button on the new debate input page to the entry page

--- DebateTimerApp/NewDebateInputPage.swift
+++ DebateTimerApp/NewDebateInputPage.swift
@@ -37,5 +37,8 @@ class NewDebateInputPage: UIViewController, UIPickerViewDataSource, UIPickerView
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return pickerData[row]
     }
+    @IBAction func BackButton(_ sender: AnyObject) {
+        
+    }
     
 }
