a0cc549faacd5c6614fd8483171cd981178a0e8b
BenGoffman <ben@goffman.com>
2017-02-03 10:50:54 -0600

    GUI
    Finished the GUI and connected the main storyboard to the coding section

---
+++ DebateTimerApp/Individual_Evaluation.swift
@@ -0,0 +1,54 @@
+//
+//  Individual_Evaluation.swift
+//  DebateTimerApp
+//
+//  Created by GOFFMAN, BENJAMIN on 2/3/17.
+//  Copyright © 2017 GOVINDARAJAN, SATHYANARAYANAN. All rights reserved.
+//
+
+import UIKit
+
+class Individual_Evaluation: UIViewController, UITextViewDelegate {
+
+    @IBOutlet weak var CommentsBoxNeg: UITextView!
+    @IBOutlet weak var CommentsBoxAff: UITextView!
+    @IBOutlet weak var Stu1CommentAff: UITextView!
+    @IBOutlet weak var Stu1CommentNeg: UITextView!
+    
+    var keyboardHeight: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        CommentsBoxNeg!.delegate = self
+        CommentsBoxAff!.delegate = self
+        Stu1CommentAff!.delegate = self
+        Stu1CommentNeg.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(OpenStateNegGUI.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        
+    }
+    
+    func keyboardWillShow(notification:NSNotification)
+    {
+        let userInfo:NSDictionary = notification.userInfo! as NSDictionary
+        let keyboardFrame:NSValue = userInfo.value(forKey: UIKeyboardFrameEndUserInfoKey) as! NSValue
+        let keyboardRectangle = keyboardFrame.cgRectValue
+        self.keyboardHeight = Int(keyboardRectangle.height)
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView)
+    {
+        self.view.frame = self.view.frame.offsetBy(dx: 0, dy: -(CGFloat(keyboardHeight)))
+        textView.text = ""
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView)
+    {
+        self.view.frame = self.view.frame.offsetBy(dx: 0, dy: CGFloat(keyboardHeight))
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
