522a48a308445f9a77e8b978141b64fe5c6386ca
JacobFl <jacob.flaum@gmail.com>
2017-02-15 10:26:04 -0600

    IDK need to commit

--- DebateTimerApp/NewDebateInputPage.swift
+++ DebateTimerApp/NewDebateInputPage.swift
@@ -86,5 +86,54 @@ class NewDebateInputPage: UIViewController, UIPickerViewDataSource, UIPickerView
         defaults.set(savedData, forKey: debateName.text!)
         
     }
+    // Call this method somewhere in your view controller setup code.
+    - (void)registerForKeyboardNotifications
+    {
+    [[NSNotificationCenter defaultCenter] addObserver:self
+    selector:@selector(keyboardWasShown:)
+    name:UIKeyboardDidShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+    selector:@selector(keyboardWillBeHidden:)
+    name:UIKeyboardWillHideNotification object:nil];
+    
+    }
+    
+    // Called when the UIKeyboardDidShowNotification is sent.
+    - (void)keyboardWasShown:(NSNotification*)aNotification
+    {
+    NSDictionary* info = [aNotification userInfo];
+    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize.height, 0.0);
+    scrollView.contentInset = contentInsets;
+    scrollView.scrollIndicatorInsets = contentInsets;
+    
+    // If active text field is hidden by keyboard, scroll it so it's visible
+    // Your app might not need or want this behavior.
+    CGRect aRect = self.view.frame;
+    aRect.size.height -= kbSize.height;
+    if (!CGRectContainsPoint(aRect, activeField.frame.origin) ) {
+    [self.scrollView scrollRectToVisible:activeField.frame animated:YES];
+    }
+    }
+    
+    // Called when the UIKeyboardWillHideNotification is sent
+    - (void)keyboardWillBeHidden:(NSNotification*)aNotification
+    {
+    UIEdgeInsets contentInsets = UIEdgeInsetsZero;
+    scrollView.contentInset = contentInsets;
+    scrollView.scrollIndicatorInsets = contentInsets;
+    }
+    
+    - (void)textFieldDidBeginEditing:(UITextField *)textField
+    {
+    activeField = textField;
+    }
+    
+    - (void)textFieldDidEndEditing:(UITextField *)textField
+    {
+    activeField = nil;
+    }
     
 }
