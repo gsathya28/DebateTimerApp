6a78451f878acc68ac78cfb114380f9669c3cd47
gsathya28 <gsathya28@gmail.com>
2017-02-28 11:32:27 -0600

    Added individual Score variable to debater class
    With encoder and decoder methods

--- DebateTimerApp/debater.swift
+++ DebateTimerApp/debater.swift
@@ -13,6 +13,7 @@ class debater: NSObject, NSCoding {
     // MARK: Properties
     var name: String?
     var stance: String?
+    var individualScore: Int?
     
     // MARK: Initializer
     init(inName: String, inStance: String)
@@ -24,11 +25,13 @@ class debater: NSObject, NSCoding {
     required init(coder aDecoder: NSCoder) {
         stance = aDecoder.decodeObject(forKey: "stance") as? String
         name = (aDecoder.decodeObject(forKey: "name") as? String)!
+        individualScore = aDecoder.decodeObject(forKey: "individualScore") as? Int
     }
     
     func encode(with aCoder: NSCoder) {
         aCoder.encode(stance, forKey: "stance")
         aCoder.encode(name, forKey: "name")
+        aCoder.encode(individualScore, forKey: "individualScore")
     }
 
     
