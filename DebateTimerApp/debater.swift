a3cd298318e01ab9774ef4acc9b5d7d8288c5deb
gsathya28 <gsathya28@gmail.com>
2017-02-28 11:29:32 -0600

    Got rid of debateScores and Times Array

--- DebateTimerApp/debater.swift
+++ DebateTimerApp/debater.swift
@@ -13,8 +13,6 @@ class debater: NSObject, NSCoding {
     // MARK: Properties
     var name: String?
     var stance: String?
-    var debateScores: [String: Int]?
-    var debateTimes: [String: Int]?
     
     // MARK: Initializer
     init(inName: String, inStance: String)
@@ -25,15 +23,11 @@ class debater: NSObject, NSCoding {
     
     required init(coder aDecoder: NSCoder) {
         stance = aDecoder.decodeObject(forKey: "stance") as? String
-        debateScores = (aDecoder.decodeObject(forKey: "scores") as? [String: Int])!
-        debateTimes = aDecoder.decodeObject(forKey: "times") as? [String: Int]!
         name = (aDecoder.decodeObject(forKey: "name") as? String)!
     }
     
     func encode(with aCoder: NSCoder) {
         aCoder.encode(stance, forKey: "stance")
-        aCoder.encode(debateScores, forKey: "scores")
-        aCoder.encode(debateTimes, forKey: "times")
         aCoder.encode(name, forKey: "name")
     }
 
