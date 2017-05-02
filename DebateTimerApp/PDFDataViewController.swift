0b258523bedcedece15ac3bf2507b1a10e0f9be5
GOFFMAN <783071@ehs-s-232-06.district196.org>
2017-05-02 10:51:43 -0500

    email
    Set up the email function for the iPad

--- DebateTimerApp/PDFDataViewController.swift
+++ DebateTimerApp/PDFDataViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class PDFDataViewController: UIViewController {
+class PDFDataViewController: UIViewController, MFMailComposeViewControllerDelegate {
     
     @IBOutlet var webView: UIWebView!
     
@@ -26,6 +27,37 @@ class PDFDataViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //goes to the email screen when the send button is tapped
+    @IBAction func SendEmail(_ sender: Any) {
+        let mailComposeViewController = configuredMailComposeViewController()
+        //makes sure it can send the email
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
+    }
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["someone@somewhere.com"])
+        mailComposerVC.setSubject("Sending you an in-app e-mail...")
+        mailComposerVC.setMessageBody("Sending e-mail in-app is not so bad!", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
+        sendMailErrorAlert.show()
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate Method
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
     }
     
 
