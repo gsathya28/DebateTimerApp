254f48810bac0cc68daacb16fdd53a6ee6f9c789
Sathyanarayanan Govindarajan <gsathya28@gmail.com>
2017-05-09 10:59:45 -0500

    Revert "Merge branch 'IDOS-Part-3-cuz-Part-2-died'"
    This reverts commit fc0d1040fa8088c6e7f9b24e61312ff6658ab22e, reversing
    changes made to 5e459188e0cc3b786b518bb2cf7d5bebc561d9ac.

--- TimerPageGUI.swift
+++ TimerPageGUI.swift
@@ -46,8 +46,11 @@ class TimerPageGUI: UIViewController, UITextViewDelegate, UIPickerViewDataSource
     @IBOutlet var save: UIButton!
     
     // Data Save Variables
+    let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    var currentDebate: debate?
+    var roundCounter: Int?
     var round: debateRound?
-    
+    var ArchiveURLCurrent: URL?
     @IBOutlet weak var rubricText: UILabel!
     
     // Load Stuff
@@ -59,6 +62,7 @@ class TimerPageGUI: UIViewController, UITextViewDelegate, UIPickerViewDataSource
         let defaults = UserDefaults.standard
         let id = defaults.object(forKey: "current") as? String
         ArchiveURLCurrent = DocumentsDirectory.appendingPathComponent(id!)
+        
         roundCounter = defaults.object(forKey: "roundCounter") as? Int
         
        
