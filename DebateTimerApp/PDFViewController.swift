e4c5c4d1f59c1b0696bf73be20bea08507d4341c
BenGoffman <783071@ehs-s-232-06.district196.org>
2017-05-19 11:01:16 -0500

    attachment and unwind
    Attached the PDF to the email and connected the back button on the pdf
    page to the debate data page

--- DebateTimerApp/PDFViewController.swift
+++ DebateTimerApp/PDFViewController.swift
@@ -43,9 +43,46 @@ class PDFViewController: UIViewController, MFMailComposeViewControllerDelegate {
         let mailComposerVC = MFMailComposeViewController()
         mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
         
-        mailComposerVC.setToRecipients(["ben@goffman.com, gsathya28@gmail.com"])
-        mailComposerVC.setSubject("PDF Test")
-        mailComposerVC.setMessageBody("I NEED THIS TO WORK!!!!", isHTML: false)
+        mailComposerVC.setToRecipients(["mitchell.snobeck@district196.org"])
+        mailComposerVC.setSubject("Debates")
+        mailComposerVC.setMessageBody("PDF summary of debate...", isHTML: false)
+        
+        let html = "<b>Hello <i>World!</i></b> <p>Generate PDF file from HTML in Swift</p>"
+        let fmt = UIMarkupTextPrintFormatter(markupText: html)
+        
+        // 2. Assign print formatter to UIPrintPageRenderer
+        
+        let render = UIPrintPageRenderer()
+        render.addPrintFormatter(fmt, startingAtPageAt: 0)
+        
+        // 3. Assign paperRect and printableRect
+        
+        let page = CGRect(x: 0, y: 0, width: 595.2, height: 841.8) // A4, 72 dpi
+        let printable = page.insetBy(dx: 0, dy: 0)
+        
+        render.setValue(NSValue(cgRect: page), forKey: "paperRect")
+        render.setValue(NSValue(cgRect: printable), forKey: "printableRect")
+        
+        // 4. Create PDF context and draw
+        
+        pdfData = NSMutableData()
+        UIGraphicsBeginPDFContextToData(pdfData as! NSMutableData, CGRect.zero, nil)
+        
+        for i in 1...render.numberOfPages {
+            
+            UIGraphicsBeginPDFPage();
+            let bounds = UIGraphicsGetPDFContextBounds()
+            render.drawPage(at: i - 1, in: bounds)
+        }
+        
+        UIGraphicsEndPDFContext();
+        
+        // 5. Save PDF file
+        
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        
+        pdfData?.write(toFile: "\(documentsPath)/file.pdf", atomically: true)
+
         
         mailComposerVC.addAttachmentData(pdfData! as Data, mimeType: "application/pdf", fileName: "debate.pdf" )
         
