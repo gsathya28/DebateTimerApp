1d12c29c6a3ba12225e027bfdf5b465710a836be
Sathyanarayanan Govindarajan <gsathya28@gmail.com>
2017-05-25 15:31:08 -0500

    Revert #3 (actually) - the branch merges are done

--- DebateData.swift
+++ DebateData.swift
@@ -23,35 +23,4 @@ class DebateData: UIViewController {
 
     @IBAction func unwindToDebateData(_sender: UIStoryboardSegue) {
     }
-    
-    @IBAction func makePDF(_ sender: Any) {
-        
-        let html = "<b>Hello <i>World!</i></b> <p>Generate PDF file from HTML in Swift</p>"
-        let fmt = UIMarkupTextPrintFormatter(markupText: html)
-        let render = UIPrintPageRenderer()
-        render.addPrintFormatter(fmt, startingAtPageAt: 0)
-        let page = CGRect(x: 0, y: 0, width: 595.2, height: 841.8) // A4, 72 dpi
-        let printable = page.insetBy(dx: 0, dy: 0)
-        render.setValue(NSValue(cgRect: page), forKey: "paperRect")
-        render.setValue(NSValue(cgRect: printable), forKey: "printableRect")
-        let pdfData = NSMutableData()
-        UIGraphicsBeginPDFContextToData(pdfData, CGRect.zero, nil)
-        
-        for i in 1...render.numberOfPages {
-            
-            UIGraphicsBeginPDFPage();
-            let bounds = UIGraphicsGetPDFContextBounds()
-            render.drawPage(at: i - 1, in: bounds)
-        }
-        
-        UIGraphicsEndPDFContext();
-        
-        // 5. Save PDF file
-        // let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
-        // let ArchiveURLCurrent = DocumentsDirectory.appendingPathComponent("banana")
-        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
-        
-        pdfData.write(toFile: "\(documentsPath)/\(currentDebate?.name).pdf", atomically: true)
-    }
-    
 }
